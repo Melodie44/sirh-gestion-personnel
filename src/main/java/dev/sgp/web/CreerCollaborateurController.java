01ded0876c9fd00c6f3623edd9fc7dcdcec8d512
Mélodie MAUBOIS <maubois.melodie@orange.fr>
2017-12-13 17:47:11 +0100

    TP03 formulaire pas fini

---
+++ src/main/java/dev/sgp/web/CreerCollaborateurController.java
@@ -0,0 +1,53 @@
+package dev.sgp.web;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.util.Properties;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dev.sgp.entite.Collaborateur;
+import dev.sgp.service.CollaborateurService;
+import dev.sgp.util.Constantes;
+
+public class CreerCollaborateurController extends HttpServlet{
+
+	// recuperation du service
+	private CollaborateurService collabService = Constantes.COLLAB_SERVICE;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String nom = req.getParameter("nom");
+		String prenom = req.getParameter("prenom");
+		String dateNaissance = req.getParameter("dateNaissance");
+		String adresse = req.getParameter("adresse");
+		String numSecuSociale = req.getParameter("numSecuSociale");
+		
+		Properties prop = new Properties();
+		prop.load(new FileInputStream("sirth-gestion-personnel.properties"));
+		String suffixe = prop.getProperty("suffixeEmailPro");
+		
+		Collaborateur collab = new Collaborateur();
+		collab.setNom(nom);
+		collab.setPrenom(prenom);
+		collab.setDateNaissance(LocalDate.parse(dateNaissance));
+		collab.setAdresse(adresse);
+		collab.setNumSecuSociale(numSecuSociale);
+		collab.setActif(true);
+		collab.setDateHeureCreation(ZonedDateTime.now());
+		collab.setMatricule("M"+numSecuSociale.substring(10, 14));
+		collab.setEmailPro(prenom+"."+nom+"@"+suffixe);
+		
+		collabService.sauvegarderCollaborateur(collab);
+		
+		req.getRequestDispatcher("/WEB-INF/views/collab/nouveauCollaborateur.jsp").forward(req, resp);
+	
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/collab/nouveauCollaborateur.jsp
@@ -0,0 +1,45 @@
+<%@ page language="java" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>SGP - App</title>
+<link rel="stylesheet"
+	href="<%=request.getContextPath()%>/bootstrap-3.3.7-dist/css/bootstrap.css">
+<script
+	src="<%=request.getContextPath()%>/bootstrap-3.3.7-dist/css/bootstrap.js"></script>
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+</head>
+
+<body>
+	<div class="container">
+		<h1>Nouveau Collaborateur</h1>
+
+		<form >
+			<div class="form-group">
+				<label for="nom">Nom *</label> 
+				<input type="text" class="form-control" id="nom" required>
+			</div>
+			<div class="form-group">
+				<label for="prenom">Prénom *</label> 
+				<input type="text" class="form-control" id="prenom" required>
+			</div>
+			<div class="form-group">
+				<label for="dateNaissance">Date de naissance *</label> 
+				<input type="date" class="form-control" id="dateNaissance" required>
+			</div>
+			<div class="form-group">
+				<label for="adresse">Adresse *</label>
+				<textarea class="form-control" rows="4" id="adresse" required></textarea>
+			</div>
+			<div class="form-group">
+				<label for="numSecuSociale">Numéro de sécurité sociale *</label> 
+				<input type="number" class="form-control" id="numSecuSociale" min="15" max="15" required>
+			</div>
+			<button type="submit" class="btn btn-default">Créer</button>
+		</form>
+	</div>
+</body>
+</html>
